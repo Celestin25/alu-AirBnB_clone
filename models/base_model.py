5ef5dd573ae9e1f4c04d8bd45c00843790539859
Celestin HAKORIMANA <celestin@DESKTOP-L0GB8TB.localdomain>
2023-01-25 11:47:00 +0200

    task

--- models/base_model.py
+++ models/base_model.py
@@ -1,20 +1,44 @@
 #!/usr/bin/python3
 """
-    Contains the definition of the BaseModel class thst defines all common
-    attributes and methods for other classes in the AirBnB clone project.
+This is the base model that contains serial/deserial information
 """
-import uuid
 from datetime import datetime
-import models
+import uuid
+from models import storage
 
 
 class BaseModel():
-    """Definition of the class BaseModel. Defines all common attributes and
-       methods for other classes in the AirBnB project
-    """
+    """ Defines all common attributes/methods for other classes """
     def __init__(self, *args, **kwargs):
-        """Initialize a new instance of the BaseModel class.
-           Also recreate a class instance from a dictiornary.
-        Attributes:
-            id (str) - a unique identification number for each class instance
-            created_at (datetime) - a datetime object indicatingt
+        """ Initializes the instances attributes """
+        if kwargs:
+            date_format = "%Y-%m-%dT%H:%M:%S.%f"
+            k_dict = kwargs.copy()
+            del k_dict["__class__"]
+            for key in k_dict:
+                if (key == "created_at" or key == "updated_at"):
+                    k_dict[key] = datetime.strptime(k_dict[key], date_format)
+            self.__dict__ = k_dict
+        else:
+            self.id = str(uuid.uuid4())
+            self.created_at = datetime.today()
+            self.updated_at = datetime.today()
+            storage.new(self)
+
+    def __str__(self):
+        """ Prints object in friendly format"""
+        return "[{}] ({}) {}".format(self.__class__.__name__,
+                                     self.id, self.__dict__)
+
+    def save(self):
+        """ Updates update_at """
+        self.updated_at = datetime.today()
+        storage.save()
+
+    def to_dict(self):
+        """ Generate a new dict with an extra field __class__ """
+        new_dict = self.__dict__.copy()
+        new_dict["__class__"] = self.__class__.__name__
+        new_dict["created_at"] = self.created_at.isoformat()
+        new_dict["updated_at"] = self.updated_at.isoformat()
+        return new_dict
