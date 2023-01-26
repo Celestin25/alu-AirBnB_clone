afc601ed6980948c26c558c7e7f7793eccfe4d14
Celestin HAKORIMANA <celestin@DESKTOP-L0GB8TB.localdomain>
2023-01-26 18:34:52 +0200

    task

--- models/base_model.py
+++ models/base_model.py
@@ -1,44 +1,28 @@
-#!/usr/bin/python3
-"""
-This is the base model that contains serial/deserial information
-"""
-from datetime import datetime
 import uuid
-from models import storage
+from datetime import datetime
 
-
-class BaseModel():
-    """ Defines all common attributes/methods for other classes """
+class BaseModel:
     def __init__(self, *args, **kwargs):
-        """ Initializes the instances attributes """
+        self.id = str(uuid.uuid4())
+        self.created_at = datetime.now()
+        self.updated_at = datetime.now()
+        
         if kwargs:
-            date_format = "%Y-%m-%dT%H:%M:%S.%f"
-            k_dict = kwargs.copy()
-            del k_dict["__class__"]
-            for key in k_dict:
-                if (key == "created_at" or key == "updated_at"):
-                    k_dict[key] = datetime.strptime(k_dict[key], date_format)
-            self.__dict__ = k_dict
-        else:
-            self.id = str(uuid.uuid4())
-            self.created_at = datetime.today()
-            self.updated_at = datetime.today()
-            storage.new(self)
-
-    def __str__(self):
-        """ Prints object in friendly format"""
-        return "[{}] ({}) {}".format(self.__class__.__name__,
-                                     self.id, self.__dict__)
-
+            kwargs["created_at"] = datetime.strptime(kwargs["created_at"], '%Y-%m-%dT%H:%M:%S.%f')
+            kwargs["updated_at"] = datetime.strptime(kwargs["updated_at"], '%Y-%m-%dT%H:%M:%S.%f')
+            del kwargs["__class__"]
+            self.__dict__.update(kwargs)
+        
     def save(self):
-        """ Updates update_at """
-        self.updated_at = datetime.today()
-        storage.save()
-
+        self.updated_at = datetime.now()
+        
     def to_dict(self):
-        """ Generate a new dict with an extra field __class__ """
-        new_dict = self.__dict__.copy()
-        new_dict["__class__"] = self.__class__.__name__
-        new_dict["created_at"] = self.created_at.isoformat()
-        new_dict["updated_at"] = self.updated_at.isoformat()
-        return new_dict
+        rdict = self.__dict__.copy()
+        rdict["created_at"] = self.created_at.isoformat()
+        rdict["updated_at"] = self.updated_at.isoformat()
+        rdict["__class__"] = self.__class__.__name__
+        return rdict
+        
+    def __str__(self):
+        clname = self.__class__.__name__
+        return "[{}] ({}) {}".format(clname, self.id, self.__dict__)
