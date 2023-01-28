53eadfa26dca52b095355117fde9f41306170be7
Celestin HAKORIMANA <celestin@DESKTOP-L0GB8TB.localdomain>
2023-01-28 11:52:45 +0200

    task

--- models/amenity.py
+++ models/amenity.py
@@ -1,13 +1,5 @@
 #!/usr/bin/python3
-"""
-    contains the definition class Amenity
-"""
-from models.base_model import BaseModel
-
 
 class Amenity(BaseModel):
-    """Definition of class Amenity that inherits from class BaseModel
-    Attr:
-        name (string): name of the amenities available at the Place
-    """
-    name = ""
+    def __init__(self):
+        self.name = ""
