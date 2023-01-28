b9d600cdbd47aa8230934cdc849575248087d69f
Celestin HAKORIMANA <celestin@DESKTOP-L0GB8TB.localdomain>
2023-01-28 13:55:50 +0200

    task

--- tests/test_models/test_review.py
+++ tests/test_models/test_review.py
@@ -1,27 +1,35 @@
 #!/usr/bin/python3
-"""
-    Contains the definition of tests for class Review
-"""
+
+"""Unit testing for the Review class."""
+
 import unittest
+
 from models.review import Review
+from models.base_model import BaseModel
 
+class TestReview(unittest.TestCase):
+"""Test cases for the Review class."""
+def test_instance_creation(self):
+    """Tests if an instance of the class can be created."""
+    review = Review()
+    self.assertIsInstance(review, Review)
 
-class TestReviewMethods(unittest.TestCase):
-    """Definition of tests for class Review"""
+def test_class_type(self):
+    """Tests if the object created is of the correct class type."""
+    review = Review()
+    self.assertEqual(str(type(review)), "<class 'models.review.Review'>")
 
-    def test_attributes_exist(self):
-        """Test that class Review has the required attributes and methods"""
-        self.assertTrue(hasattr(Review, 'place_id'))
-        self.assertTrue(hasattr(Review, 'user_id'))
-        self.assertTrue(hasattr(Review, 'text'))
+def test_inheritance(self):
+    """Tests if the class inherits from the BaseModel class."""
+    review = Review()
+    self.assertTrue(issubclass(type(review), BaseModel))
 
-    def test_Review_attributes(self):
-        """Test whether the attributes of class Review are of the right type"""
-        review_1 = Review()
-        self.assertIsInstance(review_1.place_id, str)
-        self.assertIsInstance(review_1.user_id, str)
-        self.assertIsInstance(review_1.text, str)
-
-
-if __name__ == '__main__':
-    unittest.main()
+def test_attributes(self):
+    """Tests if the class has the correct attributes."""
+    review = Review()
+    self.assertIsNotNone(review.id)
+    self.assertEqual(review.text, "")
+    self.assertEqual(review.user_id, "")
+    self.assertEqual(review.place_id, "")
+    if name == "main":
+unittest.main()
