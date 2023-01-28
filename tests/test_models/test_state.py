89263cf74ded9bcecbb660d19c53b9d99be7429e
Celestin HAKORIMANA <celestin@DESKTOP-L0GB8TB.localdomain>
2023-01-28 13:59:21 +0200

    task

--- tests/test_models/test_state.py
+++ tests/test_models/test_state.py
@@ -1,23 +1,33 @@
 #!/usr/bin/python3
-"""
-    Contains the definition of tests for class State
-"""
+
+"""Unit testing for the State class."""
+
 import unittest
+
 from models.state import State
+from models.base_model import BaseModel
 
+class TestState(unittest.TestCase):
+"""Test cases for the State class."""
+def test_instance_creation(self):
+    """Tests if an instance of the class can be created."""
+    state = State()
+    self.assertIsInstance(state, State)
 
-class TestStateMethods(unittest.TestCase):
-    """Definition of tests for class State"""
+def test_class_type(self):
+    """Tests if the object created is of the correct class type."""
+    state = State()
+    self.assertEqual(str(type(state)), "<class 'models.state.State'>")
 
-    def test_attributes_exist(self):
-        """Test that class User has the required attributes and methods"""
-        self.assertTrue(hasattr(State, 'name'))
+def test_inheritance(self):
+    """Tests if the class inherits from the BaseModel class."""
+    state = State()
+    self.assertTrue(issubclass(type(state), BaseModel))
 
-    def test_State_attributes(self):
-        """Test whether the attributes of class State are of the right type"""
-        state_1 = State()
-        self.assertIsInstance(state_1.name, str)
-
-
-if __name__ == '__main__':
-    unittest.main()
+def test_attributes(self):
+    """Tests if the class has the correct attributes."""
+    state = State()
+    self.assertIsNotNone(state.id)
+    self.assertEqual(state.name, "")
+    if name == "main":
+unittest.main()
