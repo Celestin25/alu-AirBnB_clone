10947efe76e41fb181a3b26fcbff8b93d1ac77bc
Celestin HAKORIMANA <celestin@DESKTOP-L0GB8TB.localdomain>
2023-01-28 12:04:28 +0200

    task

--- models/place.py
+++ models/place.py
@@ -1,33 +1,15 @@
 #!/usr/bin/python3
-"""
-    Contains the definition of the class Place
-"""
-from models.base_model import BaseModel
-
 
 class Place(BaseModel):
-    """Definition of class Place that inherits from class BaseModel
-    Attr:
-        city_id (string): ID of the city the place is in
-        user_id (string): ID of the person who's put up the place on AirBnb
-        name (string): name of the place
-        description (string): Description of the place
-        number_rooms (int): number of rooms
-        number_bathrooms (int): number of bathrooms
-        max_guest (int): maximum number of guests the place can host
-        price_by_night (int): price to hire the place per night
-        latitude (float): latitude coordinate of the place
-        longitude (float): longitude coordinate of the place
-        amenity_ids (list): a list of amenities (Amenity.id)
-    """
-    city_id = ""
-    user_id = ""
-    name = ""
-    description = ""
-    number_rooms = 0
-    number_bathrooms = 0
-    max_guest = 0
-    price_by_night = 0
-    latitude = 0.0
-    longitude = 0.0
-    amenity_ids = []
+    def __init__(self):
+        self.city_id = ""
+        self.user_id = ""
+        self.name = ""
+        self.description = ""
+        self.number_rooms = 0
+        self.number_bathrooms = 0
+        self.max_guest = 0
+        self.price_by_night = 0
+        self.latitude = 0.0
+        self.longitude = 0.0
+        self.amenity_ids = []
