1d0a9aa7fa9449f3d2df52de95636ee5f74d8957
Celestin HAKORIMANA <celestin@DESKTOP-L0GB8TB.localdomain>
2023-01-28 11:57:10 +0200

    task

--- models/city.py
+++ models/city.py
@@ -1,15 +1,6 @@
 #!/usr/bin/python3
-"""
-    Contains the definition of class City
-"""
-from models.base_model import BaseModel
-
 
 class City(BaseModel):
-    """Definition of class City that inherits from class BaseModel
-    Attr:
-        state_id (string): the state's id in which the city is located
-        name (string): name of the city
-    """
-    state_id = ""
-    name = ""
+    def __init__(self):
+        self.state_id = ""
+        self.name = ""
