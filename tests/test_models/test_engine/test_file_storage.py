fe9ce86f536ee0a6437b3f24cd87ac2d171af911
Celestin HAKORIMANA <celestin@DESKTOP-L0GB8TB.localdomain>
2023-01-28 14:17:42 +0200

    task

--- tests/test_models/test_engine/test_file_storage.py
+++ tests/test_models/test_engine/test_file_storage.py
@@ -1,58 +1,81 @@
-#!/usr/bin/python3
-"""
-    Contains the tests for the FileStorage class
-"""
+import os
+import json
 import unittest
-from models import storage
 from models.engine.file_storage import FileStorage
 from models.base_model import BaseModel
-import os
 
+class TestStorage(unittest.TestCase):
+    """Test cases for FileStorage class."""
+    file_location = 'file.json'
 
-class TestFileStorage(unittest.TestCase):
-    """Definition of test class for class FileStorage"""
     def setUp(self):
-        """Sets up the resources required to run tests"""
-        if os.path.isfile('my_file.json'):
-            os.rename('my_file.json', 'tmp_file.json')
-        self.model1 = BaseModel()
+        self.storage = FileStorage()
 
     def tearDown(self):
-        """Tears down the resources that have been used to run tests"""
-        if os.path.isfile('my_file.json'):
-            os.remove('my_file.json')
-        if os.path.isfile('tmp_file.json'):
-            os.rename('tmp_file.json', 'my_file.json')
-        del self.model1
+        if os.path.exists(self.file_location):
+            os.remove(self.file_location)
+        del self.storage
 
-    def test_attributes_exist(self):
-        """Test that FileStorage class has required attributes and methods"""
-        self.assertTrue(hasattr(FileStorage, '_FileStorage__objects'))
-        self.assertTrue(hasattr(FileStorage, '_FileStorage__file_path'))
-        self.assertTrue(hasattr(FileStorage, 'all'))
-        self.assertTrue(hasattr(FileStorage, 'new'))
-        self.assertTrue(hasattr(FileStorage, 'save'))
-        self.assertTrue(hasattr(FileStorage, 'reload'))
+    def test_if_instance(self):
+        """Test if storage is an instance of FileStorage"""
+        self.assertIsInstance(self.storage, FileStorage)
 
-    def test_attributes(self):
-        """Test whether the type of FileStorage class attributes is correct"""
-        self.assertEqual(storage._FileStorage__file_path, 'my_file.json')
-        self.assertIsInstance(storage._FileStorage__objects, dict)
+    def test_class_type(self):
+        """Test if storage is of class FileStorage"""
+        self.assertEqual(str(type(self.storage)), "<class 'models.engine.file_storage.FileStorage'>")
 
-    def test_all(self):
-        """Test that the all method returns the correct dictionary"""
-        my_dict = storage.all()
-        my_id = 'BaseModel.' + self.model1.id
-        self.assertIsInstance(my_dict, dict)
-        self.assertIn(my_id, my_dict)
+    def test_file_creation(self):
+        """Test if file.json is created after calling save method"""
+        self.assertFalse(os.path.exists(self.file_location))
+        self.storage.save()
+        self.assertTrue(os.path.exists(self.file_location))
 
-    def test_reload(self):
-        """Test that the reload method actually reloads objects from file"""
-        self.model1.save()
-        self.assertTrue(os.path.isfile('my_file.json'))
-        tmp_obj = BaseModel()
-        tmp_id = 'BaseModel.' + tmp_obj.id
-        tmp_obj.save()
-        del storage._FileStorage__objects[tmp_id]
+    def test_add_object(self):
+        """Test if object is added to storage"""
+        self.assertEqual(self.storage.all(), {})
+        base = BaseModel()
+        self.storage.new(base)
+        self.assertIn(base, self.storage.all().values())
+
+    def test_type_of_all(self):
+        """Test if all method returns a dictionary"""
+        self.assertEqual(type(self.storage.all()), dict)
+
+    def test_reload_method(self):
+        """Test if reload method returns a dictionary"""
+        self.storage.reload()
+        self.assertIsInstance(self.storage.all(), type({}))
+
+    def test_clear_and_reload(self):
+        """Test if reload method returns an empty dictionary after clearing all method"""
+        storage = FileStorage()
+        storage.all().clear()
         storage.reload()
-        self.assertIn(tmp_id, storage.all())
+        self.assertEqual({}, self.storage.all())
+        del storage
+
+class TestStorageReload(unittest.TestCase):
+    """Test reload method again"""
+    file_location = 'file.json'
+
+    def setUp(self):
+        self.storage = FileStorage()
+
+    def tearDown(self):
+        if os.path.exists(self.file_location):
+            os.remove(self.file_location)
+        del self.storage
+
+    def test_reload_with_object(self):
+        """Test reload method with object saved"""
+        base = BaseModel()
+        base.save()
+
+        self.storage.reload()
+
+        object_key = "{}.{}".format(base.__class__.__name__, base.id)
+        self.assertIn(object_key, self.storage.all())
+
+        self.assertEqual(self.storage.all()[object_key].id, base.id)
+
+if __name__ ==
