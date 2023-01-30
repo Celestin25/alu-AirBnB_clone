ee0492369c598d92a6f86a3f3b12b5892bf6dd19
Celestin HAKORIMANA <celestin@DESKTOP-L0GB8TB.localdomain>
2023-01-30 11:45:38 +0200

    task

--- console.py
+++ console.py
@@ -1,58 +1,19 @@
-import argparse
-import pickle
-from models import storage
-from models.base_model import BaseModel
-from models.user import User
-from models.state import State
-from models.city import City
-from models.place import Place
-from models.amenity import Amenity
-from models.review import Review
+import cmd
 
-class HBNBCommand:
-    """Defines command interpreter.
-    """
-
-    __all_classes = {
-        "BaseModel",
-        "User",
-        "State",
-        "City",
-        "Place",
-        "Amenity",
-        "Review"
-    }
-
-    def __init__(self):
-        self.parser = argparse.ArgumentParser(description='HBNB Command Line Interface')
-        self.parser.add_argument('class_name', choices=self.__all_classes,
-                                 help='Name of the class to create instance')
-        self.parser.add_argument('id', help='ID of the class instance')
-
-    def create(self):
-        """
-            Creates a new instance of a class,
-            saves it (to the file) and prints the id.
-        """
-        args = self.parser.parse_args()
-        new_object = eval(args.class_name + "()")
-        new_object.save()
-        print(new_object.id)
-        with open('storage.pkl', 'wb') as f:
-            pickle.dump(storage, f)
-
-    def show(self):
-        """
-        Display string representation of a class instance of given id.
-        """
-        args = self.parser.parse_args()
-        objdict = storage.all()
-        key = args.class_name + '.' + args.id
-        if key in objdict:
-            print(objdict[key])
-        else:
-            print("** no instance found **")
-
-    def do_quit(self):
-        """Quit command -> exit the program."""
+class HBNBCommand(cmd.Cmd):
+    prompt = "(hbnb) "
+    
+    def do_quit(self, line):
+        """Quit command to exit the program"""
         return True
+    
+    def do_EOF(self, line):
+        """EOF command to exit the program"""
+        return True
+    
+    def emptyline(self):
+        """Do not execute anything on an empty line"""
+        pass
+    
+if __name__ == '__main__':
+    HBNBCommand().cmdloop()
