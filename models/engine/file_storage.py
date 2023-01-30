cd90ab5596b37bac841661715a6db214c201ba23
Celestin HAKORIMANA <celestin@DESKTOP-L0GB8TB.localdomain>
2023-01-30 14:59:12 +0200

    task

--- models/engine/file_storage.py
+++ models/engine/file_storage.py
@@ -1,29 +1,49 @@
+#!/usr/bin/python3
 import json
 import os
-from datetime import datetime
+import models
+
 
 class FileStorage:
-    __file_path = 'file.json'
+
+    __file_path = 'hbnb.json'
     __objects = {}
 
     def all(self):
+    
         return FileStorage.__objects
 
     def new(self, obj):
-        key = obj.__class__.__name__ + '.' + obj.id
-        FileStorage.__objects[key] = obj
+        
+        FileStorage.__objects[obj.id] = obj
 
     def save(self):
-        with open(FileStorage.__file_path, 'w', encoding='utf-8') as f:
-            d = {}
-            for key, value in FileStorage.__objects.items():
-                d[key] = value.to_dict()
-            f.write(json.dumps(d))
+    
+        new_dict = {}
+        for key in FileStorage.__objects.keys():
+            new_dict[key] = FileStorage.__objects[key].to_json()
+        with open(FileStorage.__file_path, 'w+', encoding='utf-8') as fn:
+            json.dump(new_dict, fn)
 
     def reload(self):
-        if os.path.isfile(FileStorage.__file_path):
-            with open(FileStorage.__file_path, 'r', encoding='utf-8') as f:
-                FileStorage.__objects = json.loads(f.read())
-                for key, value in FileStorage.__objects.items():
-                    k = key.split('.')
-                    FileStorage.__objects[key] = eval(k[0])(**value)
+    
+        if os.path.isfile(FileStorage.__file_path) is True:
+            with open(FileStorage.__file_path, 'r+', encoding='utf-8') as fn:
+                obj = json.load(fn)
+                for key in obj.keys():
+                    is_dict = obj[key]
+                    is_class = is_dict['__class__']
+                    if 'BaseModel' in is_class:
+                        FileStorage.__objects[key] = models.BaseModel(obj[key])
+                    if 'Amenity' in is_class:
+                        FileStorage.__objects[key] = models.Amenity(obj[key])
+                    if 'City' in is_class:
+                        FileStorage.__objects[key] = models.City(obj[key])
+                    if 'Place' in is_class:
+                        FileStorage.__objects[key] = models.Place(obj[key])
+                    if 'Review' in is_class:
+                        FileStorage.__objects[key] = models.Review(obj[key])
+                    if 'State' in is_class:
+                        FileStorage.__objects[key] = models.State(obj[key])
+                    if 'User' in is_class:
+                        FileStorage.__objects[key] = models.User(obj[key])
