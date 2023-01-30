cbe1ced04098f777eda85ec068f82a5aa77e8fe8
Celestin HAKORIMANA <celestin@DESKTOP-L0GB8TB.localdomain>
2023-01-30 14:13:35 +0200

    task

--- models/engine/file_storage.py
+++ models/engine/file_storage.py
@@ -1,33 +1,25 @@
 import json
+import os
 
 class FileStorage:
-    __file_path = "file.json"
-    __objects = {}
+__file_path = "file.json"
+__objects = {}
+def all(self):
+    return FileStorage.__objects
 
-    def all(self):
-        """Returns the dictionary __objects"""
-        return self.__objects
+def new(self, obj):
+    FileStorage.__objects[obj.__class__.__name__ + "." + obj.id] = obj
 
-    def new(self, obj):
-        """Sets in __objects the obj with key <obj class name>.id"""
-        key = "{}.{}".format(type(obj).__name__, obj.id)
-        self.__objects[key] = obj
+def save(self):
+    with open(FileStorage.__file_path, mode="w", encoding="utf-8") as f:
+        d = {key: val.to_dict() for key, val in FileStorage.__objects.items()}
+        json.dump(d, f)
 
-    def save(self):
-        """Serializes __objects to the JSON file (path: __file_path)"""
-        with open(self.__file_path, mode='w', encoding='utf-8') as f:
-            to_dump = {}
-            for key, value in self.__objects.items():
-                to_dump[key] = value.to_dict()
-            json.dump(to_dump, f)
-
-    def reload(self):
-        """Deserializes the JSON file to __objects (only if the JSON file (__file_path) exists ;
-        otherwise, do nothing. If the file doesn’t exist, no exception should be raised)"""
-        try:
-            with open(self.__file_path, encoding='utf-8') as f:
-                to_load = json.load(f)
-                for key, value in to_load.items():
-                    self.__objects[key] = type(value)(**value)
-        except FileNotFoundError:
-            pass
+def reload(self):
+    if os.path.exists(FileStorage.__file_path):
+        with open(FileStorage.__file_path, encoding="utf-8") as f:
+            d = json.load(f)
+            for key, val in d.items():
+                cls_name, obj_id = key.split(".")
+                val["__class__"] = cls_name
+                FileStorage.__objects[key] = eval(cls_name)(**val)
