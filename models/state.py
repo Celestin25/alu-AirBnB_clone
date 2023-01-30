cbc2c34718483c28948a450678f236e81a304d4c
Celestin HAKORIMANA <celestin@DESKTOP-L0GB8TB.localdomain>
2023-01-30 17:46:14 +0200

    task

--- models/state.py
+++ models/state.py
@@ -1,4 +1,11 @@
 #!/usr/bin/python3
+from models.base_model import BaseModel
+
+
 class State(BaseModel):
-    def __init__(self):
-        self.name = ""
+
+    name = ''
+
+    def __init__(self, *args, **kwargs):
+    
+        super().__init__(*args, **kwargs)
