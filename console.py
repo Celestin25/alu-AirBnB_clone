42a75889a0d6843087cf9728ba2122af295e26d6
Celestin HAKORIMANA <celestin@DESKTOP-L0GB8TB.localdomain>
2023-01-30 11:52:25 +0200

    task

--- console.py
+++ console.py
@@ -1,19 +1,15 @@
 import cmd
 
 class HBNBCommand(cmd.Cmd):
-    prompt = "(hbnb) "
+    prompt = '(hbnb) '
     
-    def do_quit(self, line):
+    def do_quit(self, arg):
         """Quit command to exit the program"""
         return True
     
-    def do_EOF(self, line):
+    def do_EOF(self, arg):
         """EOF command to exit the program"""
         return True
-    
-    def emptyline(self):
-        """Do not execute anything on an empty line"""
-        pass
-    
+
 if __name__ == '__main__':
     HBNBCommand().cmdloop()
