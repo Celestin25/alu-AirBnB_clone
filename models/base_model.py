14aed5461fb60c60bd4f06c4b6a771ce09efb9a9
Celestin HAKORIMANA <celestin@DESKTOP-L0GB8TB.localdomain>
2023-01-30 14:03:54 +0200

    task

--- models/base_model.py
+++ models/base_model.py
@@ -1,28 +1,24 @@
-import uuid
-from datetime import datetime
+import models.engine.file_storage
+
+storage = models.engine.file_storage.FileStorage()
 
 class BaseModel:
-    def __init__(self, *args, **kwargs):
-        self.id = str(uuid.uuid4())
-        self.created_at = datetime.now()
-        self.updated_at = datetime.now()
-        
-        if kwargs:
-            kwargs["created_at"] = datetime.strptime(kwargs["created_at"], '%Y-%m-%dT%H:%M:%S.%f')
-            kwargs["updated_at"] = datetime.strptime(kwargs["updated_at"], '%Y-%m-%dT%H:%M:%S.%f')
-            del kwargs["__class__"]
-            self.__dict__.update(kwargs)
-        
+    ...
+
     def save(self):
+        """Calls save method of storage and update updated_at attribute"""
         self.updated_at = datetime.now()
-        
-    def to_dict(self):
-        rdict = self.__dict__.copy()
-        rdict["created_at"] = self.created_at.isoformat()
-        rdict["updated_at"] = self.updated_at.isoformat()
-        rdict["__class__"] = self.__class__.__name__
-        return rdict
-        
-    def __str__(self):
-        clname = self.__class__.__name__
-        return "[{}] ({}) {}".format(clname, self.id, self.__dict__)
+        storage.save()
+
+    def __init__(self, *args, **kwargs):
+        ...
+        if kwargs:
+            for key, value in kwargs.items():
+                if key == "created_at" or key == "updated_at":
+                    value = datetime.strptime(value, "%Y-%m-%dT%H:%M:%S.%f")
+                if key != "__class__":
+                    setattr(self, key, value)
+        else:
+            self.id = str(uuid4())
+            self.created_at = self.updated_at = datetime.now()
+            storage.new(self)
