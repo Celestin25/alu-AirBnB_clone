a3171bf4e99e0b5ba507ab3022b933b24d1d0027
Celestin HAKORIMANA <celestin@DESKTOP-L0GB8TB.localdomain>
2023-01-30 17:35:21 +0200

    task

--- models/city.py
+++ models/city.py
@@ -1,6 +1,12 @@
 #!/usr/bin/python3
+from models.base_model import BaseModel
+
 
 class City(BaseModel):
-    def __init__(self):
-        self.state_id = ""
-        self.name = ""
+
+    state_id = ''
+    name = ''
+
+    def __init__(self, *args, **kwargs):
+    
+        super().__init__(*args, **kwargs)
