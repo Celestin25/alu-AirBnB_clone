dde0cc9c769fcac65a35eb02b4e71c4dac79caa5
Celestin HAKORIMANA <celestin@DESKTOP-L0GB8TB.localdomain>
2023-01-30 17:06:22 +0200

    task

--- tests/test_models/test_state.py
+++ tests/test_models/test_state.py
@@ -1,33 +1,36 @@
 #!/usr/bin/python3
-
-"""Unit testing for the State class."""
-
 import unittest
-
 from models.state import State
-from models.base_model import BaseModel
+import datetime
+
 
 class TestState(unittest.TestCase):
-"""Test cases for the State class."""
-def test_instance_creation(self):
-    """Tests if an instance of the class can be created."""
-    state = State()
-    self.assertIsInstance(state, State)
 
-def test_class_type(self):
-    """Tests if the object created is of the correct class type."""
-    state = State()
-    self.assertEqual(str(type(state)), "<class 'models.state.State'>")
+    def setUp(self):
+    
+        self.test_model1 = State()
+        self.test_model2 = State()
 
-def test_inheritance(self):
-    """Tests if the class inherits from the BaseModel class."""
-    state = State()
-    self.assertTrue(issubclass(type(state), BaseModel))
+    def test_basic_setup(self):
+    
+        self.assertTrue(hasattr(self.test_model1, "name"))
+        self.assertFalse(hasattr(self.test_model1, "first_name"))
+        self.assertTrue(self.test_model1.id != self.test_model2.id)
+        m1c = self.test_model1.created_at
+        m2c = self.test_model2.created_at
+        self.assertTrue(m1c != m2c)
+        self.assertTrue(type(m1c) is datetime.datetime)
 
-def test_attributes(self):
-    """Tests if the class has the correct attributes."""
-    state = State()
-    self.assertIsNotNone(state.id)
-    self.assertEqual(state.name, "")
-    if name == "main":
-unittest.main()
+    def test_types(self):
+    
+        self.assertTrue(type(self.test_model1.name) is str)
+
+    def test_save(self):
+    
+        m1u = self.test_model1.updated_at
+        self.test_model1.save()
+        m1u_saved = self.test_model1.updated_at
+        self.assertFalse(m1u == m1u_saved)
+
+if __name__ == '__main__':
+    unittest.main()
