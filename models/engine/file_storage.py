64fa147305d0367f83e1a0b0714e0ea696d1ed15
Celestin HAKORIMANA <celestin@DESKTOP-L0GB8TB.localdomain>
2023-01-30 13:58:36 +0200

    task

--- models/engine/file_storage.py
+++ models/engine/file_storage.py
@@ -5,21 +5,29 @@ class FileStorage:
     __objects = {}
 
     def all(self):
+        """Returns the dictionary __objects"""
         return self.__objects
 
     def new(self, obj):
-        self.__objects["{}.{}".format(obj.__class__.__name__, obj.id)] = obj
+        """Sets in __objects the obj with key <obj class name>.id"""
+        key = "{}.{}".format(type(obj).__name__, obj.id)
+        self.__objects[key] = obj
 
     def save(self):
-        with open(self.__file_path, 'w') as f:
-            f.write(json.dumps(self.__objects))
+        """Serializes __objects to the JSON file (path: __file_path)"""
+        with open(self.__file_path, mode='w', encoding='utf-8') as f:
+            to_dump = {}
+            for key, value in self.__objects.items():
+                to_dump[key] = value.to_dict()
+            json.dump(to_dump, f)
 
     def reload(self):
+        """Deserializes the JSON file to __objects (only if the JSON file (__file_path) exists ;
+        otherwise, do nothing. If the file doesn’t exist, no exception should be raised)"""
         try:
-            with open(self.__file_path, 'r') as f:
-                self.__objects = json.loads(f.read())
-        except:
+            with open(self.__file_path, encoding='utf-8') as f:
+                to_load = json.load(f)
+                for key, value in to_load.items():
+                    self.__objects[key] = type(value)(**value)
+        except FileNotFoundError:
             pass
-
-storage = FileStorage()
-storage.reload()
