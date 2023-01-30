08a300cc53110f1522f0fb31b59424bb4139b12f
Celestin HAKORIMANA <celestin@DESKTOP-L0GB8TB.localdomain>
2023-01-30 16:51:54 +0200

    task

--- tests/test_models/test_base_model.py
+++ tests/test_models/test_base_model.py
@@ -1,52 +1,57 @@
-import json
-import os
+#!/usr/bin/python3
 import unittest
-import time
-from datetime import datetime
-from models import storage
-from models.base_model import BaseModel
-from models.engine.file_storage import FileStorage
-from io import StringIO
-from unittest.mock import patch
+from models import BaseModel
+import datetime
 
-class TestBaseModelMethods(unittest.TestCase):
+
+class TestBaseModel(unittest.TestCase):
+
     def setUp(self):
-        self.test_model = BaseModel()
-    
-    def test_instance(self):
-        self.assertIsInstance(self.test_model, BaseModel)
+        
+        self.test_model1 = BaseModel()
+        self.test_model2 = BaseModel()
 
-    def test_class(self):
-        self.assertEqual(str(type(self.test_model)), "<class 'models.base_model.BaseModel'>")
+    def test_basic_setup(self):
+    
+        self.assertTrue(hasattr(self.test_model1, "id"))
+        self.assertTrue(hasattr(self.test_model1, "__class__"))
+        self.assertTrue(hasattr(self.test_model1, "created_at"))
+        self.assertTrue(hasattr(self.test_model1, "updated_at"))
+        self.assertTrue(self.test_model1.id != self.test_model2.id)
+        m1c = self.test_model1.created_at
+        m2c = self.test_model2.created_at
+        self.assertTrue(m1c != m2c)
+
+    def test_types(self):
+    
+        self.assertTrue(type(self.test_model1.id) is str)
+        self.assertTrue(type(self.test_model1.__class__) is type)
+        m1c = self.test_model1.created_at
+        m2c = self.test_model2.created_at
+        m1u = self.test_model1.updated_at
+        m2u = self.test_model2.updated_at
+        self.assertTrue(type(m1c) is datetime.datetime)
+        self.assertTrue(type(m2c) is datetime.datetime)
+        self.assertTrue(type(m1u) is datetime.datetime)
+        self.assertTrue(type(m2u) is datetime.datetime)
 
     def test_save(self):
-        original_time = self.test_model.updated_at
-        time.sleep(1)
-        self.test_model.save()
-        self.assertNotEqual(original_time, self.test_model.updated_at)
-        self.assertTrue(self.test_model.updated_at > original_time)
+        
+        m1u = self.test_model1.updated_at
+        self.test_model1.save()
+        m1u_saved = self.test_model1.updated_at
+        self.assertFalse(m1u == m1u_saved)
 
-    def test_save_file(self):
-        if os.path.isfile("file.json"):
-            os.remove("file.json")
-        self.test_model.save()
-        self.assertTrue(os.path.isfile("file.json"))
-        with open("file.json", 'r') as file:
-            serialized_content = json.load(file)
-            for item in serialized_content.values():
-                self.assertIsNotNone(item['__class__'])
+    def test_to_json(self):
+    
+        testmodelid = self.test_model1.id
+        jsondict = self.test_model1.to_json()
+        self.assertNotEqual(jsondict, self.test_model1.__dict__)
+        self.assertEqual(jsondict["id"], self.test_model1.__dict__["id"])
+        self.assertNotEqual(jsondict["created_at"],
+                            self.test_model1.__dict__["created_at"])
+        self.assertNotEqual(type(jsondict["created_at"]),
+                            type(self.test_model1.__dict__["created_at"]))
 
-    def test_string_representation(self):
-        with patch("sys.stdout", new=StringIO()) as fake_out:
-            print(self.test_model)
-            self.assertEqual(fake_out.getvalue(),
-                             "[{}] ({}) {}\n".format(self.test_model.__class__.__name__,
-                                                     self.test_model.id,
-                                                     self.test_model.__dict__))
-
-    def test_to_dict(self):
-        dict_representation = self.test_model.to_dict()
-        self.assertTrue(dict_representation['__class__'] == self.test_model.__class__.__name__)
-
-if __name__ == "__main__":
+if __name__ == '__main__':
     unittest.main()
