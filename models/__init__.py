316dc070649d0735daff10c9011344c91984c178
Celestin HAKORIMANA <celestin@DESKTOP-L0GB8TB.localdomain>
2023-01-30 13:46:53 +0200

    task

--- models/__init__.py
+++ models/__init__.py
@@ -1,9 +1,4 @@
-#!/usr/bin/python3
-"""
-    init file for the models package
-"""
-from .engine.file_storage import FileStorage
-
+from models.engine.file_storage import FileStorage
 
 storage = FileStorage()
 storage.reload()
