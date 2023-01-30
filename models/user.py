b813a22ceff2840c9b0ef721ed377ad25e4f5a42
Celestin HAKORIMANA <celestin@DESKTOP-L0GB8TB.localdomain>
2023-01-30 17:48:42 +0200

    task

--- models/user.py
+++ models/user.py
@@ -1,7 +1,14 @@
 #!/usr/bin/python3
+from models.base_model import BaseModel
+
+
 class User(BaseModel):
-    def __init__(self):
-        self.email = ""
-        self.password = ""
-        self.first_name = ""
-        self.last_name = ""
+
+    email = ''
+    password = ''
+    first_name = ''
+    last_name = ''
+
+    def __init__(self, *args, **kwargs):
+    
+        super().__init__(*args, **kwargs)
