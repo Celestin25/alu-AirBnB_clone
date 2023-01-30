39570bdc3e5f18a186a514a4bf2161b3a6d977c1
Celestin HAKORIMANA <celestin@DESKTOP-L0GB8TB.localdomain>
2023-01-30 13:43:59 +0200

    task

--- models/engine/file_storage.py
+++ models/engine/file_storage.py
@@ -1,33 +1,25 @@
 import json
 
 class FileStorage:
-    file_path = 'file.json'
-    objects = {}
+    __file_path = "file.json"
+    __objects = {}
 
-    @classmethod
-    def all(cls):
-        return cls.objects
+    def all(self):
+        return self.__objects
 
-    @classmethod
-    def new(cls, obj):
-        key = f'{obj.__class__.__name__}.{obj.id}'
-        cls.objects[key] = obj
+    def new(self, obj):
+        self.__objects["{}.{}".format(obj.__class__.__name__, obj.id)] = obj
 
-    @classmethod
-    def save(cls):
-        object_dict = {}
-        for obj in cls.objects:
-            object_dict[obj] = cls.objects[obj].to_dict()
-        with open(cls.file_path, 'w') as file:
-            json.dump(object_dict, file)
+    def save(self):
+        with open(self.__file_path, 'w') as f:
+            f.write(json.dumps(self.__objects))
 
-    @classmethod
-    def reload(cls):
+    def reload(self):
         try:
-            with open(cls.file_path) as file:
-                serialized_content = json.load(file)
-                for item in serialized_content.values():
-                    class_name = item['__class__']
-                    cls.new(eval(class_name + "(**" + str(item) + ")"))
-        except FileNotFoundError:
+            with open(self.__file_path, 'r') as f:
+                self.__objects = json.loads(f.read())
+        except:
             pass
+
+storage = FileStorage()
+storage.reload()
