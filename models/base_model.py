adc2abf682afd366b9641d9756754a4b2105836e
Celestin HAKORIMANA <celestin@DESKTOP-L0GB8TB.localdomain>
2023-01-30 17:30:39 +0200

    task

--- models/base_model.py
+++ models/base_model.py
@@ -1,28 +1,46 @@
+#!/usr/bin/python3
+from datetime import datetime, date, time
 import uuid
-from datetime import datetime
+import models
 
-class BaseModel:
+
+class BaseModel():
+
     def __init__(self, *args, **kwargs):
-        self.id = str(uuid.uuid4())
-        self.created_at = datetime.now()
-        self.updated_at = datetime.now()
-
-        if kwargs:
-            kwargs["created_at"] = datetime.strptime(kwargs["created_at"], '%Y-%m-%dT%H:%M:%S.%f')
-            kwargs["updated_at"] = datetime.strptime(kwargs["updated_at"], '%Y-%m-%dT%H:%M:%S.%f')
-            del kwargs["__class__"]
-            self.__dict__.update(kwargs)
+    
+        if len(args) > 0:
+            if type(args[0]) is dict:
+                self.__dict__ = args[0]
+                self.__dict__['created_at'] = datetime.strptime(
+                    (self.__dict__['created_at']), "%Y-%m-%d %H:%M:%S.%f")
+                self.__dict__['updated_at'] = datetime.strptime(
+                    (self.__dict__['updated_at']), "%Y-%m-%d %H:%M:%S.%f")
+        else:
+            self.id = str(uuid.uuid4())
+            self.created_at = datetime.now()
+            self.updated_at = datetime.now()
+            models.storage.new(self)
 
     def save(self):
+    
         self.updated_at = datetime.now()
+        models.storage.save()
 
-    def to_dict(self):
-        rdict = self.__dict__.copy()
-        rdict["created_at"] = self.created_at.isoformat()
-        rdict["updated_at"] = self.updated_at.isoformat()
-        rdict["__class__"] = self.__class__.__name__
-        return rdict
+    def to_json(self):
+    
+        my_dict = self.__dict__
+        new_dict = {}
+        for i in my_dict.keys():
+            if (isinstance(my_dict[i], datetime)):
+                new_dict[i] = str(my_dict[i])
+            else:
+                new_dict[i] = my_dict[i]
+        new_dict['__class__'] = self.__class__.__name__
+        return new_dict
 
     def __str__(self):
-        clname = self.__class__.__name__
-        return "[{}] ({}) {}".format(clname, self.id, self.__dict__)
+    
+        class_name = self.__class__.__name__
+        id_string = self.id
+        my_dict = self.__dict__
+        return ("[{}] ({}) {}".format(class_name, id_string, my_dict))
