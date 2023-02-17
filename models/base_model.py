e8bcf6aa6dc872904610ca08d0520e6573f2c998
Mugisha-Beline <belinemugisha21@gmail.com>
2023-02-17 16:16:15 +0200

    basemodel

--- models/base_model.py
+++ models/base_model.py
@@ -1,46 +1,58 @@
-#!/usr/bin/python3
-from datetime import datetime, date, time
+#!/usr/bin/pyhon3
+"""
+Parent class that will inherit
+"""
 import uuid
-import models
+from datetime import datetime
+from models import storage
 
 
-class BaseModel():
-
+class BaseModel:
+    """Defines all common attributes/methods
+    """
     def __init__(self, *args, **kwargs):
-    
-        if len(args) > 0:
-            if type(args[0]) is dict:
-                self.__dict__ = args[0]
-                self.__dict__['created_at'] = datetime.strptime(
-                    (self.__dict__['created_at']), "%Y-%m-%d %H:%M:%S.%f")
-                self.__dict__['updated_at'] = datetime.strptime(
-                    (self.__dict__['updated_at']), "%Y-%m-%d %H:%M:%S.%f")
-        else:
+        """initializes all attributes
+        """
+        if not kwargs:
             self.id = str(uuid.uuid4())
             self.created_at = datetime.now()
-            self.updated_at = datetime.now()
-            models.storage.new(self)
-
-    def save(self):
-    
-        self.updated_at = datetime.now()
-        models.storage.save()
-
-    def to_json(self):
-    
-        my_dict = self.__dict__
-        new_dict = {}
-        for i in my_dict.keys():
-            if (isinstance(my_dict[i], datetime)):
-                new_dict[i] = str(my_dict[i])
-            else:
-                new_dict[i] = my_dict[i]
-        new_dict['__class__'] = self.__class__.__name__
-        return new_dict
+            self.updated_at = self.created_at
+            storage.new(self)
+        else:
+            f = "%Y-%m-%dT%H:%M:%S.%f"
+            for key, value in kwargs.items():
+                if key == 'created_at' or key == 'updated_at':
+                    value = datetime.strptime(kwargs[key], f)
+                if key != '__class__':
+                    setattr(self, key, value)
 
     def __str__(self):
-    
-        class_name = self.__class__.__name__
-        id_string = self.id
-        my_dict = self.__dict__
-        return ("[{}] ({}) {}".format(class_name, id_string, my_dict))
+        """returns class name, id and attribute dictionary
+        """
+        class_name = "[" + self.__class__.__name__ + "]"
+        dct = {k: v for (k, v) in self.__dict__.items() if (not v) is False}
+        return class_name + " (" + self.id + ") " + str(dct)
+
+    def save(self):
+        """updates last update time
+        """
+        self.updated_at = datetime.now()
+        storage.save()
+
+    def to_dict(self):
+        """creates a new dictionary, adding a key and returning
+        datemtimes converted to strings
+        """
+        new_dict = {}
+
+        for key, values in self.__dict__.items():
+            if key == "created_at" or key == "updated_at":
+                new_dict[key] = values.strftime("%Y-%m-%dT%H:%M:%S.%f")
+            else:
+                if not values:
+                    pass
+                else:
+                    new_dict[key] = values
+        new_dict['__class__'] = self.__class__.__name__
+
+        return new_dict
